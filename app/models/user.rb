18ee2cadb3a62957151de6adb74b0324526f64e8
Alisa Tatarinova & Michael Yeung <pair+alisatee.myeung58@devbootcamp.com>
2014-08-28 11:21:16 -0700

    user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
   # Remember to create a migration!
+  has_many :tweets
+  validates_uniqueness_of :username, :email
+  # validates_presence_of :username, :password
+
 end
