39e6dbe113e24e880854de57df6f165cfb05ec38
Alisa Tatarinova & Michael Yeung <pair+alisatee.myeung58@devbootcamp.com>
2014-08-28 12:17:59 -0700

    user authentication

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -1,7 +1,25 @@
-get '/' do
-  if logged_in?
-    erb :"users/homepage"
-  else
-    erb :"users/signin_signup"
-  end
+get '/signin' do
+  erb :"users/signin"
 end
+
+post '/signin' do
+  @user = User.find_by(username: params[:username])
+  session[:user_id] = @user.id
+  redirect '/'
+end
+
+get '/signup' do
+  erb :"users/signup"
+end
+
+post '/signup' do
+  User.create(params)
+  # consider signing the user in after signing up
+  redirect '/'
+end
+
+get '/logout' do
+  session[:user_id] = nil
+  redirect '/'
+end
+
