6ad8baf1b29f2b41783cf108e09afb7b1e972652
Matt Darin <mgd2233@gmail.com>
2014-08-28 11:20:27 -0700

    created model for tweets

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,16 @@
-User.delete_all
+# User.delete_all
 Tweet.delete_all
 
-10.times do
-  User.create(
-    first_name: Faker::Name.first_name,
-    last_name: Faker::Name.last_name,
-    username: Faker::Internet.user_name,
-    password: Faker::Commerce.color,
-    email: Faker::Internet.email,
-    about: Faker::Lorem.sentence,
-    location: Faker::Address.city)
-end
+# 10.times do
+#   User.create(
+#     first_name: Faker::Name.first_name,
+#     last_name: Faker::Name.last_name,
+#     username: Faker::Internet.user_name,
+#     password: Faker::Commerce.color,
+#     email: Faker::Internet.email,
+#     about: Faker::Lorem.sentence,
+#     location: Faker::Address.city)
+# end
 
 20.times do
   Tweet.create(
