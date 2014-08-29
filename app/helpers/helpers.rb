4b9edd6455f2fb257fe877787724d5bdd9d5cf28
Matthew Darin <mgd2233@gmail.com>
2014-08-28 17:43:34 -0700

    completed gravatar method

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,11 +1,16 @@
 helpers do
-  def current_user
-    if session[:user_id]
-      @current_user ||= User.find(session[:user_id])
-    end
-  end
+	def current_user
+		if session[:user_id]
+			@current_user ||= User.find(session[:user_id])
+		end
+	end
 
-  def logged_in?
-    !current_user.nil?
-  end
+	def avatar_url(email)
+		gravatar_id = Digest::MD5.hexdigest(email)
+		"http://gravatar.com/avatar/#{gravatar_id}"
+	end  
+
+	def logged_in?
+		!current_user.nil?
+	end
 end
